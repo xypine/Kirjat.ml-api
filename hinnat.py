34f06b43b487ae017b5981d6e4a51ffad26cca27
jonnelafin <elias.eelinen@gmail.com>
2020-12-02 17:01:02 +0200

    Niiiice

--- hinnat.py
+++ hinnat.py
@@ -16,6 +16,8 @@ app_start_wait = 0
 # Store variables
 store_url = "https://kauppa.jamera.net"
 store_url_search = "https://kauppa.jamera.net/kauppa/haku/?q="
+store_lang = "fi-FI"
+store_lang_short = "fi"
 
 
 # Is the file already present?
@@ -26,13 +28,17 @@ def is_file_already_present(file):
 
 # Getting the html                                        
 def request(url):
-    response = requests.get(url)
+    headers = {"Accept-Language": "fi-FI,fi;q=0.8,en-US;q=0.5,en;q=0.3"} #{"Accept-Language": store_lang + "," + store_lang_short + ";q=0.1"} #fi-FI,fi;q=0.8,en-US;q=0.5,en;q=0.3
+    response = requests.get(url, headers=headers)
     page_html = response.text
     page_soup = soup(page_html, 'html.parser')
     return page_soup
 
 
 def get_products(page_soup, verbose=False):
+    if len(page_soup.find_all('table', {'class': 'tuotteet_flex'})) < 1:
+        print("Failed to find the product container from the provided HTML, returning an empty array.")
+        return []
     table = page_soup.find_all('table', {'class': 'tuotteet_flex'})[0]
     containers = table.findAll("tr", recursive=False)
     if verbose:
@@ -41,16 +47,47 @@ def get_products(page_soup, verbose=False):
     for container in containers:
         product_images = container.find_all('table', {'class': 'kuva'})
         product_name = container.find_all('a', {'class': 'otsikko'}, recursive=True)
-        tuotteet.append(kirja("Yeet", 99, "kuva"))
+        product_possible_price = container.find_all('td', {'class': 'radio'})
+
+        price = -1
+        prices = []
+        condition = ""
+        conditions = []
+        for radio in product_possible_price:
+            product_price = radio.find_all('label')
+            if len(product_price) > 0:
+                price_sub = product_price[0]
+                condition_possible = price_sub.find_all('p', {'class': 'name'})
+                if len(condition_possible) > 0:
+                    c = condition_possible[0].text
+                    condition = c
+                    conditions.append(c)
+                price_possible = price_sub.find_all('span')
+                if len(price_possible) > 0:
+                    try:
+                        p = int(price_possible[0].text.replace("€", "").replace(",", ""))
+                        price = p # 13,30 € -> 1330
+                        prices.append(p)
+                    except Exception as e:
+                        price = -2
+        name = ""
+        if len(product_name) > 0:
+            name = product_name[0].text
+        tuotteet.append(kirja(name, price, "kuva"))
     return tuotteet
 
 def scrape():
     global store_url, store_url_search
     print("Getting the HTML...")
-    soup = request(store_url_search + "Nos vemos! 3")
+    soup = request(store_url_search + "Bios1 Elämä ja evoluutio")
     print("Getting the HTML...OK")
     print("Parsing the HTML for products...")
-    print( get_products(soup) )
+    products = get_products(soup, True)
+    if products == []:
+        print("Apparently no products were found, here is the HTML for debugging:")
+        print(soup)
+    for i in products:
+        print(i)
     print("Parsing the HTML for products...OK")
 
 if __name__ == "__main__":
--- kirja.py
+++ kirja.py
@@ -1,10 +1,14 @@
 class kirja:
     image = ""
     price = 0
-    name = "Ei Nimeä?"
-    def __init__(self, name="", price=0, image=""):
+    name = "Ei Nimeä?
+    prices = []
+    conditions = []
+    def __init__(self, name="", price=0, prices = [], conditions = [], image=""):
         self.name = name
         self.price = price
+        self.prices = prices
+        self.conditions = conditions
         self.image = image
     def __str__(self):
-        return "Kirja(" + self.name + ":" + self.price
+        return "Kirja(" + self.name + ":" + str(self.price) + ")"
