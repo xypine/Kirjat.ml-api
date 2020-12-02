b0daf8545e47289f699a1b2e77ddf9c440da9a55
jonnelafin <elias.eelinen@gmail.com>
2020-12-02 18:38:41 +0200

    NICE

--- hinnat.py
+++ hinnat.py
@@ -1,7 +1,10 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
 # Script written by Elias Eskelinen aka Jonnelafin
 
 # import the libraries
 from bs4 import BeautifulSoup as soup
+from urllib.parse import urlencode, quote_plus
 import requests
 import os, time, sys
 
@@ -28,8 +31,9 @@ def is_file_already_present(file):
 
 # Getting the html                                        
 def request(url):
+    clean_url = url
     headers = {"Accept-Language": "fi-FI,fi;q=0.8,en-US;q=0.5,en;q=0.3"} #{"Accept-Language": store_lang + "," + store_lang_short + ";q=0.1"} #fi-FI,fi;q=0.8,en-US;q=0.5,en;q=0.3
-    response = requests.get(url, headers=headers)
+    response = requests.get(clean_url, headers=headers)
     page_html = response.text
     page_soup = soup(page_html, 'html.parser')
     return page_soup
@@ -79,30 +83,58 @@ def get_products(page_soup, verbose=False):
 def scrape():
     global store_url, store_url_search
     print("Getting the HTML...")
-    soup = request(store_url_search + "Bios1 Elämä ja evoluutio")
+    book = "Tekijä Pitkä matematiikka 3"
+    booko = "Tekijä Pitkä matematiikka 3"
+    book = book.replace(u'ä', "%E4")
+    book = book.replace(u'ö', "%F6")
+    soup = request(store_url_search + book)
     print("Getting the HTML...OK")
     print("Parsing the HTML for products...")
     products = get_products(soup, True)
     if products == []:
         print("Apparently no products were found, here is the HTML for debugging:")
         print(soup)
+    ind = 0
     for i in products:
-        print(i)
+        diff = set(i.name).difference(set(booko))
+        dif = len(diff) + ind*4
+        print(str(i) + " | " + str(dif))
+        ind += 1
     print("Parsing the HTML for products...OK")
 
 def scrape_from_file(filename):
     result = []
     lines = ""
-    with open(filename, "r") as f:
+    with open(filename, "r", encoding="utf-8") as f:
         lines = f.readlines()
     for i in lines:
         bookname = i.replace("\n", "")
-        soup = request(store_url_search + bookname)
+        booknameo = i.replace("\n", "")
+        bookname = bookname.replace(u'ä', "%E4")
+        bookname = bookname.replace(u'ö', "%F6")
+#        bookname = urlencode(bookname, quote_via=quote_plus)
+#        print("Encoding " + str(bookname) + "...")
+#        bookname = bookname.encode('windows-1250')
+#        bookname = bookname.decode('latin-1')
+        soup = request(store_url_search + str(bookname))
+        print("Scraping " + str(store_url_search + str(bookname)))
         products = get_products(soup, True)
+        bestDiff = 99999
+        best = kirja()
+        ind = 0
         for b in products:
             if b.price > -1:
-                result.append(b)
-                break
+                diff = set(b.name).difference(set(booknameo))
+                diff2 = abs(len(booknameo) - len(b.name))
+                dif = len(diff) + ind*4
+                if dif < bestDiff:
+                    bestDiff = dif
+                    best = b
+                    print(dif)
+#                result.append(b)
+#                break
+            ind += 1
+        result.append(best)
     return result
 if __name__ == "__main__":
     print("  _    _ _                   _               _ ")
