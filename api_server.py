d04fac43b0f4927c75c895e6e2e74e90b1c0b6e4
jonnelafin <elias.eelinen@gmail.com>
2020-12-03 17:07:00 +0200

    More heroku stuff

--- api_server.py
+++ api_server.py
@@ -42,5 +42,7 @@ def query():
         books = scrape(request.form.get('query'))
         return jsonify( {"data": booklistTodictList(books), "err" : scraper.clean(scraper.kirjat_scrape_err)} )
     return "400: Query form must contain the key \"query\"", 400
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
